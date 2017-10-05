2f75642a275124a90a13163f57668e6d08ffc30b
Hyunjung <trlee329@gmail.com>
2017-10-05 17:30:26 +0200

    make it clear

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
-(function() {
- var
-  form = $('.form'),
-  cache_width = form.width(),
-  a4 = [595.28, 841.89]; // for a4 size paper width and height
+$(document).ready(function() {
+  $('#myForm').submit(function(){
+    //alert("submitted");
+    // get data
+    var data = $(this).serialize();
+    alert(data);
 
- $('#create_pdf').on('click', function() {
-  $('body').scrollTop(0);
-  createPDF();
- });
- //create pdf
- function createPDF() {
-  getCanvas().then(function(canvas) {
-   var
-    img = canvas.toDataURL("image/png"),
-    doc = new jsPDF({
-     unit: 'px',
-     format: 'a4'
-    });
-   doc.addImage(img, 'JPEG', 20, 20);
-   doc.save('techumber-html-to-pdf.pdf');
-   form.width(cache_width);
+    //create and save PDF
+    var doc = new jsPDF();
+    doc.text(data,10,10);
+    doc.save('a4.pdf');
+
   });
- }
-
- // create canvas object
- function getCanvas() {
-  form.width((a4[0] * 1.33333) - 80).css('max-width', 'none');
-  return html2canvas(form, {
-   imageTimeout: 2000,
-   removeContainer: true
-  });
- }
-
-}());
+});
--- createPDF.md
+++
@@ -1,1 +0,0 @@
-"hm. i'm afraid this is not exactly what we need. look, there is a contact form on a Website where People can enter theier data. after they click on "Send" this data is automatically overtaken into a prepared PDF of the owner of the Website...the owener of the Website then gets this PDF (with the right data of the Clients) via mail" 
